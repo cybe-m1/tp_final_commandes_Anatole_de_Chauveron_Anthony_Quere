254eddf2958c8e6f82af9d45c9a00804d066e29a
anthony <quereantho@gmail.com>
2021-11-02 11:54:48 +0100

    :tada: First Commit

---
+++ src/main/java/com/fges/commandes/commandes/CommandesApplication.java
@@ -0,0 +1,13 @@
+package com.fges.commandes.commandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandesApplication.class, args);
+    }
+
+}
