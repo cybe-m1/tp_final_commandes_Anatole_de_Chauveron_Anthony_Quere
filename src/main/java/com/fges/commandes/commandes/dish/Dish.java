0959d6397f8d03ca5051de6cfdcacd5d2382f51b
Anatole-DC <adechauveron@gmail.com>
2021-11-09 13:30:14 +0100

    :sparkles: feat: Dish model (first version)

--- src/main/java/com/fges/commandes/commandes/dish/Dish.java
+++ src/main/java/com/fges/commandes/commandes/dish/Dish.java
@@ -1,4 +1,23 @@
 package com.fges.commandes.commandes.dish;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
 public class Dish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String description;
+    private String type;
+    private Float price;
+    private Float tva;
 }
