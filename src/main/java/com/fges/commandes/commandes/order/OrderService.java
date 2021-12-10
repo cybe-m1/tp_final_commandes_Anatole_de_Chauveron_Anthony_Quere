c71ca3bc0d7ff9d0220c27724142763bd143c89f
anthony <quereantho@gmail.com>
2021-12-10 20:45:19 +0100

    :sparkles: Add get order total amount

--- src/main/java/com/fges/commandes/commandes/order/OrderService.java
+++ src/main/java/com/fges/commandes/commandes/order/OrderService.java
@@ -7,9 +7,12 @@ import com.fges.commandes.commandes.dish.IDish;
 import com.fges.commandes.commandes.menu.IMenu;
 import com.fges.commandes.commandes.menu.Menu;
 import com.fges.commandes.commandes.menu.MenuNotFoundException;
+import com.fges.commandes.commandes.order.dto.TotalAmountResponseDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -53,6 +56,39 @@ class OrderService implements IOrder {
 
         order.getMenus().add(menu);
         return orderRepository.save(order);
+    }
+
+    public TotalAmountResponseDto getOrderAmount(Long orderId) throws OrderNotFoundException {
+        Order order = findOrderById(orderId);
+        float total = 0.0f;
+        float tva = 0.0f;
+
+        // Add dishes prices
+        for (Dish dish : order.getDishes()) {
+            total += dish.getPrice();
+            tva += dish.getPrice() * dish.getTva();
+        }
+
+        // Add menu prices
+        for (Menu menu : order.getMenus()) {
+            // Get dishes minus the smallest element
+            Set<Dish> dishes = menu.getDishes()
+                    .stream()
+                    .sorted((dish1, dish2) -> dish2.getPrice().compareTo(dish1.getPrice()))
+                    .skip(1)
+                    .collect(Collectors.toSet());
+
+            float menuPrice = 0.0f;
+
+            for (Dish dish : dishes) {
+                menuPrice += dish.getPrice() * (1 + dish.getTva());
+            }
+            total += menuPrice;
+            tva += menuPrice * menu.getTva();
+        }
+
+
+        return new TotalAmountResponseDto(total, tva, total + tva);
     }
 
 }
